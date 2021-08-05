bb1f27795c528a804d9fe447f4c34398da45e5aa
DESKTOP-TINVNIK\lky <newddong@hanmail.net>
2021-08-05 12:14:12 +0900

    s3업로드 관련 수정

--- router/post.js
+++ router/post.js
@@ -47,7 +47,7 @@ router.get("/getPostList", (req, res) => {
 	// }
 });
 
-router.post("/createPost", (req, res) => {
+router.post("/createPost",uploadS3.array('imgfile',99), (req, res) => {
 	if (req.session.user) {
         console.log("%s %s [%s] %s %s %s | createPost by %s", req.ip, new Date(), req.method, req.hostname, req.originalUrl, req.protocol, req.session.user); // prettier-ignore
 		User.model
@@ -56,13 +56,14 @@ router.post("/createPost", (req, res) => {
 			.equals(req.session.user)
 			.exec((err, user) => {
 				console.log(user);
+                console.log(req.files);
 				var post = new Post.model({
 					user: user[0],
 					user_id: user[0].nickname,
 					photo_user: user[0].profileImgUri,
 					location: req.body.location,
 					time: req.body.time,
-					images: req.body.images,
+					images: req.files.map((v,i)=>v.location),
 					content: req.body.content,
 					like: req.body.like,
 					count_comment: req.body.count_comment,
