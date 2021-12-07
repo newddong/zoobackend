620582984281cc3b352d95c6a4009f05315fb226
lky <newddong@hanmail.net>
2021-12-07 21:34:28 +0900

    [기영]유저 오브젝트 스키마 작업완료

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  tapWidth: 4,
+  printWidth: 150,
+  useTabs: true
+};
--- schema/user.js
+++ schema/user.js
@@ -1,59 +1,171 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Post = require("./post");
 
-const UserSchema = new mongoose.Schema({
-    //기본정보
-	id: {type:String},//핸드폰번호 혹은 이메일 로그인시에 사용
-	password: {type:String},
-	name: {type:String},
-    nickname:{type:String},
+/** 동의사항 */
+const UserAgreement = {
+	/** @type {boolean} 14세 이상 */
+	is_over_fourteen: {type: Boolean},
+	/** @type {boolean} 서비스 이용동의 */
+	is_service: {type: Boolean},
+	/** @type {boolean} 개인정보 제공동의 */
+	is_personal_info: {type: Boolean},
+	/** @type {boolean} 위치정보 제공동의 */
+	is_location_service_info: {type: Boolean},
+	/** @type {boolean} 기부정보 제공동의 */
+	is_donation_info: {type: Boolean},
+	/** @type {boolean} 메케팅 활용 동의 */
+	is_marketting_info: {type: Boolean},
+};
 
-    userType:{type:String}, //유저타입 : 사람(user),동물(pet),보호소(shelter)
-    idType:{type:String}, //계정타입 : 핸드폰(mobile),이메일(email)
-    
-    mobilecompany:{type:String}, //모바일일경우 통신사
-    
-    
-    reg_date:{type:Date,default:Date.now},
-    upd_date:{type:Date,default:Date.now},
-    
-    count:{
-        upload:{type:Number,default:0},
-        follower:{type:Number,default:0},
-        following:{type:Number,default:0},
-    },
+/** 유저 주소 */
+const UserAddress = {
+	/** @type {String} 시,도  */
+	city: String,
 
+	/** @type {String} 군,구 */
+	district: String,
 
-    text_intro:{type:String},
-    profileImgUri:{type:String},
-    
+	/** @type {String} 동,읍,면 */
+	neighbor: String,
+}; //회원주소
 
-    belonged_pets:[{type:Schema.Types.ObjectId}], //등록된 팻
-    volunteeractivity:[{type:Schema.Types.ObjectId}], //동물보호 활동
+/** 관심사 항목 키워드 */
+const UserInterest = {
+	/** @type {String} 지역 관심사 */
+	location: {type: String},
+	/** @type {String} 활동 관심사 */
+	activity: {type: String},
+};
 
-    deleted:{type:Boolean,default:false},
+const ShelterAddress = {
+	/** @type {String} 시,도  */
+	city: {type: String},
 
-    //보호소 관련 정보
-    shelter_name:{type:String},
-    shelter_addr:{type:String},
-    shelter_phone:{type:String},
-    shelter_email:{type:String},
-    shelter_url:{type:String},
-    shelter_foundation_date:{type:Date,default:Date.now},
+	/** @type {String} 군,구 */
+	district: {type: String},
 
-    //펫 관련 정보
-    age:{type:Number,default:0},
-    sex:{type:String},
-    adoptionType:{type:String},
-    animalKind:{type:String},
-    animalKindDetail:{type:String},
-    animalNo:{type:String},
-    owner:{type:Schema.Types.ObjectId}
-});
+	/** @type {String} 동,읍,면 */
+	neighbor: {type: String},
+
+	/** @type {String} 검색주소 */
+	brief: {type: String},
+
+	/** @type {String} 검색주소(자세히) */
+	detail: {type: String},
+};
+
+/** 스키마 명세 */
+const UserObject = {
+	/** @type {'user'|'shelter'|'pet'} 유저 타입정보 '일반유저|보호소|반려동물'로 구분됨 */
+	user_type: {type: String},
+	/** @type {UserAgreement} 가입시 동의사항 */
+	user_agreement: UserAgreement,
+	/** @type {string} 실명 */
+	user_name: {type: String},
+	/** @type {string} 닉네임 */
+	user_nickname: {type: String},
+	/** @type {string} 휴대폰번호 */
+	user_phone_number: {type: String},
+	/** @type { String} 가입된 통신사 */
+	user_mobile_company: {type: String},
+
+	/** @type { Boolean} 폰번호 인증여부 */
+	user_is_verified_phone_number: {type: Boolean},
+
+	/** @type { String} 이메일 */
+	user_email: {type: String},
+
+	/** @type { Boolean} 이메일 인증여부 */
+	user_is_verified_email: {type: Boolean},
+
+	/** @type { String} 패스워드 */
+	user_password: {type: String},
+	/** @type {UserAddress} */
+	user_address: UserAddress,
+	/** @type { String} 프로필 사진 */
+	user_profile_uri: {type: String},
+
+	/** @type { String} 프로필에 노출될 자기소개 */
+	user_introduction: {type: String},
+
+	/** @type { String} 필요한지 검토 필요 */
+	user_birthday: {type: String},
+
+	/** @type {Array.<UserInterest>} 유저의 관심사*/
+	user_interests: UserInterest,
+
+	/** @type { Number} 업로드 게시물 숫자 */
+	user_upload_count: {type: Number},
+
+	/** @type { Number} 팔로우 숫자 */
+	user_follow_count: {type: Number},
+
+	/** @type { Number} 팔로워 숫자 */
+	user_follower_count: {type: Number},
+
+	/** @type { Boolean} 유저의 차단여부 */
+	user_denied: {type: Boolean},
+
+	/** @type { Date} 가입일 */
+	user_register_date: {type: Date},
+
+	/** @type {'private'|'public'} 보호소 유형, 공립(public), 사립(private)로 나뉨 */
+	shelter_type: {type: String},
+
+	/** @type { String} 보호소 이름 */
+	shelter_name: {type: String},
+	/** @type {ShelterAddress} 보호소 주소 */
+	shelter_address: ShelterAddress,
+	/** @type { String} 보호소 홈페이지 uri */
+	shelter_homepage: {type: String},
+
+	/** @type { String} 보호소 대표 전화번호, 휴대폰 번호 */
+	shelter_delegate_contact_number: {type: String},
+
+	/** @type { Date} 보호소 설립일 */
+	shelter_foundation_date: {type: Date},
+
+	/** @type { Boolean} 반려동물이 임시보호 중인지 여부 */
+	pet_is_temp_protection: {type: Boolean},
+
+	/** @type { Boolean} 반려동물의 종류(ex 개, 고양이, 토끼 등) */
+	pet_species: {type: Boolean},
+
+	/** @type { String} 반려동물의 종류(ex 리트리버, 불독, 진돗개 등) */
+	pet_species_detail: {type: String},
+
+	/** @type { 'male'|'female'|'unknown'} 반려동물의 성별 */
+	pet_sex: {type: String},
+
+	/** @type {'yes'|'no'|'unknown'} 반려동물 중성화 여부 */
+	pet_neutralization: {type: String},
+
+	/** @type { Date} 반려동물 생일 */
+	pet_birthday: {type: Date},
+
+	/** @type { String} 반려동물 몸무게 */
+	pet_weight: {type: String},
+
+	/** @type {'protect'|'adopt'|'companion'} 반려동물의 상태, 임시보호중(protect), 입양됨(adopt), 반려동물(companion)  */
+	pet_status: {type: String},
+
+	/** @type {String } 반려동물 입양자 */
+	pet_adopter: {type: Schema.Types.ObjectId},
+
+	/** @type {String} 반려동물 임시보호자 */
+	pet_protector: {type: Schema.Types.ObjectId},
+};
+
+/** @type {mongoose.Schema} */
+const UserSchema = new mongoose.Schema(SchemaDef);
+
+const model = mongoose.model('UserObject', UserSchema);
+
+/** @type {(schema:UserObject)=>mongoose.Document} */
+const makeNewdoc = schema => {
+	return new model(schema);
+};
 
 module.exports.schema = UserSchema;
-module.exports.model = mongoose.model("user", UserSchema);
-
-
-
+module.exports.model = model;
+module.exports.makeNewdoc = makeNewdoc;
