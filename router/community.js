da9d02577fa0e94498c6b338046ce195a84c235b
hjs-desk <js.hwang@pine-partners.com>
2022-03-25 13:43:38 +0900

    [hjs] 공통코드 불러오기 오류 수정, 커뮤니티 생성 및 불러오기 초안 만듦.

--- router/community.js
+++ router/community.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Community = require('../schema/community');
 const uploadS3 = require('../common/uploadS3');
+const LikeFeed = require('../schema/likefeed');
 const {controller, controllerLoggedIn} = require('./controller');
 const {ALERT_NOT_VALID_OBJECT_ID, ALERT_NO_RESULT, ALERT_NO_MATCHING} = require('./constants');
 const mongoose = require('mongoose');
@@ -10,6 +11,47 @@ const mongoose = require('mongoose');
 router.post('/changeLocalPathToS3Path', uploadS3.array('s3path_uri'), (req, res) => {
 	controllerLoggedIn(req, res, async () => {
 		res.json({status: 200, msg: req.files});
+	});
+});
+
+// 커뮤니티 게시물 신규 작성
+router.post('/createCommunity', (req, res) => {
+	controllerLoggedIn(req, res, async () => {
+		let community = await Community.makeNewdoc({
+			community_title: req.body.community_title,
+			community_content: req.body.community_content,
+			community_location: req.body.community_location,
+			community_is_temporary: req.session.community_is_temporary,
+			community_type: req.body.community_type,
+			community_interests: req.body.community_interests,
+		});
+
+		if (req.body.community_avatar_id) {
+			feed.community_avatar_id = req.body.community_avatar_id;
+		}
+
+		let newCommunity = await community.save();
+		res.json({status: 200, msg: newCommunity});
+	});
+});
+
+//커뮤니티를 불러옴(홈화면)
+router.post('/getCommunityList', (req, res) => {
+	controller(req, res, async () => {
+		let community = await Community.model.find().populate('community_writer_id').populate('community_avatar_id').sort('-_id').lean();
+		if (!community) {
+			//res.status(404);
+			res.json({status: 404, msg: ALERT_NO_RESULT});
+			return;
+		}
+
+		res.json({
+			status: 200,
+			msg: {
+				free: community.filter(v => v.community_type == 'free'),
+				review: community.filter(v => v.community_type == 'review'),
+			},
+		});
 	});
 });
 
--- schema/community.js
+++ schema/community.js
@@ -1,10 +1,54 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const RecentComment = {
+	/** @type { String} 피드에 달린 최신 댓글의 db아이디 */
+	comment_id: {type: String},
+	/** @type { String} 댓글 작성자의 닉네임 */
+	comment_user_nickname: {type: String},
+	/** @type { String} 댓글의 내용 */
+	comment_contents: {type: String},
+};
+
 const CommunityObject = {
 	/** 일반 필드 */
 	/** @type { String} 데이터 오브젝트의 타입 */
 	type: {type: String, default: 'CommunityObject'},
+	/** @type { String} 커뮤니티 제목 */
+	community_title: {type: String},
+	/** @type { String} 커뮤니티 본문 (text, image, video 모두 html 형태로 저장) */
+	community_content: {type: String},
+	/** @type { String} 게시글 작성자의 db고유 아이디 */
+	community_writer_id: {type: Schema.Types.ObjectId, ref: 'UserObject'},
+	/** @type { String} 주인공 동물로 지정한 반려동물 계정의 id, 작성자가 avatar_id로 클라이언트에 표시됨 */
+	community_avatar_id: {type: Schema.Types.ObjectId, ref: 'UserObject'},
+
+	/** @type { String} 게시글의 작성 지역정보 */
+	community_location: {type: String},
+	/** @type { RecentComment} 게시글에 달린 최신 댓글 */
+	community_recent_comment: RecentComment,
+	/** @type { Date} 커뮤니티  최초 작성일 */
+	community_date: {type: Date, default: Date.now()},
+	/** @type { Date} 커뮤니티  최종 업데이트 날짜 */
+	community_update_date: {type: Date, default: Date.now()},
+	/** @type { Boolean} 임시저장 여부 */
+	community_is_temporary: {type: Boolean, default: false},
+	/** @type { Boolean} 삭제 여부 */
+	community_is_delete: {type: Boolean, default: false},
+	/** @type { Boolean} 추천 게시물 여부 */
+	community_is_recomment: {type: Boolean, default: false},
+
+	/** @type { String} 게시글의 타입 - 자유게시판(free) | 후기(review)  */
+	community_type: {type: String, default: 'free'},
+	/** @type { String} 관심사 항목 키워드 (공통 코드에 의해 확장 가능) */
+	community_interests: {type: String},
+
+	/** @type { Number} 게시글에 좋아요를 누른 수 */
+	community_like_count: {type: Number, default: 0},
+	/** @type { Number} 게시글을 즐겨찾기로 등록한 수 */
+	community_favorite_count: {type: Number, default: 0},
+	/** @type { Number} 게시글에 달린 댓글의 수(대댓글 포함) */
+	community_comment_count: {type: Number, default: 0},
 };
 
 /** @type {mongoose.Schema} */
