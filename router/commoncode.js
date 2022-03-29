8fbdc676afbd7e6b3c075d1df8b8833a03ae671e
hjs-desk <js.hwang@pine-partners.com>
2022-03-30 00:16:05 +0900

    [hjs] 공통 코드 정보 불러오는 출력 형식을 다양화

--- router/commoncode.js
+++ router/commoncode.js
@@ -31,7 +31,33 @@ router.post('/getCommonCodeDynamicQuery', (req, res) => {
 		for (let filed in req.body) {
 			req.body[filed] !== '' ? (query[filed] = req.body[filed]) : null;
 		}
-		result = await CommonCode.model.find(query, {_id: 0, common_code_msg_kor: 1, common_code_msg_eng: 1, common_code_category: 1}).exec();
+
+		result = await CommonCode.model
+			.find(query, {_id: 0, common_code_value: 1, common_code_msg_kor: 1, common_code_msg_eng: 1, common_code_category: 1})
+			.exec();
+
+		//언어별 메시지 셋팅
+		let common_code_msg = 'common_code_msg_' + query.common_code_language;
+
+		//출력 형식이 관심분야 일 경우 아래와 같이 출력형식을 만든다.
+		if (query.common_code_out_type == 'interests') {
+			let result_object = {};
+			for (let i = 0; i < result.length; i++) {
+				if (result[i].common_code_category == 'topic') {
+					result_object[result[i].common_code_value] = Object();
+					//언어별 키값 생성을 위해 동적 키값 할당 적용(동적 키값 생성은 []를 이용)
+					result_object[result[i].common_code_value].topic = result[i][common_code_msg];
+				}
+				//코드 정의(definition) 일경우 topic에서 생성된 하위단에 push 한 후, 출력형식 생성
+				else if (result[i].common_code_category == 'definition') {
+					if (result_object[result[i].common_code_value].definition == undefined) {
+						result_object[result[i].common_code_value].definition = Array();
+					}
+					result_object[result[i].common_code_value].definition.push(result[i][common_code_msg]);
+				}
+			}
+			result = result_object;
+		}
 		res.json({status: 200, msg: result});
 	});
 });
