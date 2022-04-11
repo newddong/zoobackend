56499254ad09975b77bca7470355179883551a75
hjs-desk <js.hwang@pine-partners.com>
2022-04-11 13:47:02 +0900

    hjs] 사용자 알림 스키마 내역 변경

--- schema/notice.js
+++ schema/notice.js
@@ -8,20 +8,19 @@ const NoticeObject = {
 	notice_update_date: {type: Date, default: Date.now()},
 	/** @type { Boolean} 전체 알림 */
 	notice_all: {type: Boolean, default: false},
-	/** @type { Boolean} 팔로워 새 게시글 알림 */
-	notice_newfollower: {type: Boolean, default: true},
-	/** @type { Boolean} 즐겨찾은 보호요청 상태 변경 알림 */
-	notice_favorite_protect_request: {type: Boolean, default: true},
+
 	/** @type { Boolean} 반려동물의 접종 예정일 알림 */
 	notice_pet_vaccination: {type: Boolean, default: true},
-	/** @type { Boolean} 내 게시글 알림 */
+
+	/** @type { Boolean} 나를 팔로우시 알림 */
+	notice_follow: {type: Boolean, default: true},
+	/** @type { Boolean} 내 게시글 알림(좋아요, 댓글) */
 	notice_my_post: {type: Boolean, default: true},
-	/** @type { Boolean} 내 게시글에 달린 댓글 알림 */
-	notice_comment_on_my_post: {type: Boolean, default: true},
 	/** @type { Boolean}  나를 태그하거나 팔로우시 알림 */
-	notice_tag_follower: {type: Boolean, default: true},
+	notice_tag: {type: Boolean, default: true},
 	/** @type { Boolean} 내 신청서 상태 변경시 알림 */
 	notice_my_applicant: {type: Boolean, default: false},
+
 	/** @type { Boolean} 공지 알림 */
 	notice_alarm: {type: Boolean, default: true},
 };
